6079219671a7190cdf2fc7a0bd5d6b1a49d78980
great <greatoladipupo818@gmail.com>
2024-06-07 08:50:56 +0100

    cors

--- server.js
+++ server.js
@@ -4,21 +4,34 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
-
+import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
+const whitelist = ['http://localhost:4200', ''];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
 const app = express();
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 
 // Swagger setup
 const swaggerDefinition = {
   openapi: '3.0.0',
   info: {
-    title: 'LMS API',
+    title: 'Allign Traits',
     version: '1.0.0',
-    description: 'API documentation for the Learning Management System',
+    description: 'API documentation for the Alligned Traits Learning Management System',
   },
   servers: [
     {
