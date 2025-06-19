b9626ce3e9b94862b63fb3e5349400c3f519e36a
Odion Julius <odionjulius7@gmail.com>
2025-06-19 18:49:06 +0100

    just the env. RESEND_API_KEY

--- src/config/resend.ts
+++ src/config/resend.ts
@@ -3,5 +3,5 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const resend = new Resend('re_L8ceAt8f_Au5p9J1y5uRULvF9BkPqAdo5');
-// export const resend = new Resend(process.env.RESEND_API_KEY);
+// export const resend = new Resend('re_L8ceAt8f_Au5p9J1y5uRULvF9BkPqAdo5');
+export const resend = new Resend(process.env.RESEND_API_KEY);
