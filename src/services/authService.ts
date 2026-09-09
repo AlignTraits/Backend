a5c19749f17f3d362ee17d7a9ee1ac13caac3458
Oluwatosin Ajigbon <samueltobi032@gmail.com>
2026-09-09 04:08:18 +0100

    Extend JWT expiration from 1 hour to 30 days

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -254,7 +254,7 @@ const loginService = async ({
     const token = jwt.sign(
       { userId: user.id },
       process.env.JWT_SECRET as string,
-      { expiresIn: '1h' }
+      { expiresIn: '30d' }
     );
     return {
       ok: true,
